f35d8f4a1be51ac78e4b5b4310106f928fc861af
Sreelekshmy <sreelekshmynair97@gmail.com>
2018-04-07 11:39:10 +0000

    added forgetpass.[php

---
+++ forgetpass.php
@@ -0,0 +1,12 @@
+<html>
+<head> <title>LICENSE MANAGEMENT SYSTEM</title>
+</head>
+<body>
+  <table>
+    <form name="form3" method="post">
+      <tr><th>Name</th><th><input type="text"  name="nme"></th></tr>
+      <tr><th>Security Question</th><th><select name = "drop"><br>
+        <option value = "SELECT">SELECT</option><br>
+        <option value = "What was your pets name">What was your pets name</option><br>
+        <option value = "Whats your favourite chldhood memory?">Whats your favourite chldhood memory?</option><br>
+        <option value = "Whats your mothers maiden name?">Whats your mothers maiden name?</option><br></select></th></tr> <br>
